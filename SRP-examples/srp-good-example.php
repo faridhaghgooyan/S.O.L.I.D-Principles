7cbc1a3b7a06300219eb1b4466e031bff6be6ac6
mrhaghgooyan <mrhaghgooyan@gmail.com>
2023-05-24 22:25:24 +0200

    SRP good and bad examples was added.

---
+++ SRP-examples/srp-good-example.php
@@ -0,0 +1,14 @@
+<?php
+class UserManager {
+    public function createUser($userData) {
+        // Create user logic
+    }
+    
+    public function deleteUser($userId) {
+        // Delete user logic
+    }
+    
+    public function updateUser($userId, $newData) {
+        // Update user logic
+    }
+}
